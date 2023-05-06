84e2a83ab9e27dcd567c988512bbc4d7f4b3d6df
Abhinav Maurya <96171326+AbhinavM71@users.noreply.github.com>
2023-05-06 20:14:19 +0530

    Create modules.v
    Signed-off-by: Abhinav Maurya <96171326+AbhinavM71@users.noreply.github.com>

---
+++ modules.v
@@ -0,0 +1,14 @@
+
+`include "adder-subtracter.v"
+`include "Multiplier.v"
+`include "divider.v"
+
+`include "addition_normaliser.v"
+`include "Multiplication_Normaliser.v"
+`include "reciprocal.v"
+`include "Booth_Multiplier.v"
+`include "Booth.v"
+
+`include "and.v"
+`include "or.v"
+`include "not.v"
